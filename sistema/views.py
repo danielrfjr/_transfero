b9239e26a6553d46fc46d61602e2a7818765b75d
Daniel Rodrigues <danielrf.rodrigues2016@gmail.com>
2025-03-24 16:47:05 -0300

    Subindo as mudanças

--- sistema/views.py
+++ sistema/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
-# Aqui irão ficar todas as viwes (controladores) ref ao sistema.
+# Aqui irão ficar todas as viwes (controladores) ref ao APP sistema.
 
-
+# A função index informa o que vai acontecer quando ela for chamada
 def index(request):
     return render(
         request,
