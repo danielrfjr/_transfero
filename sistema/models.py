2bb62ba543c51a35c7dc7229000d53071db34676
Daniel Rodrigues <danielrf.rodrigues2016@gmail.com>
2025-03-31 16:29:44 -0300

    subindo alterações

--- sistema/models.py
+++ sistema/models.py
@@ -13,28 +13,31 @@ class Usuario(models.Model):
     endereco = models.CharField(max_length=100)
     data_cadastro = models.DateTimeField(default= timezone.now)
     ativo = models.BooleanField(default = True)
+    imagem = models.ImageField(blank=True, upload_to='imagens/%Y/%m')
 
     def __str__(self):
         return f'{self.nome} {self.sobrenome}'
-
-class Filme(models.Model):
-    nome = models.CharField(max_length = 50)
-    ano = models.DateField()
-    estudio = models.CharField(max_length = 50)
-    genero = models.CharField(max_length = 50)
-    sinopse = models.TextField()
-    data_cadastro = models.DateTimeField(default= timezone.now)
-
-    def __str__(self):
-        return self.nome
 
 class Genero(models.Model):
     nome = models.CharField(max_length = 50)
     data_cadastro = models.DateTimeField(default= timezone.now)
     def __str__(self):
         return self.nome
+    
+class Filme(models.Model):
+    nome = models.CharField(max_length = 50)
+    ano = models.DateField()
+    estudio = models.CharField(max_length = 50)
+    # genero = models.CharField(max_length = 50)
+    genero = models.ForeignKey(Genero, on_delete= models.SET_NULL, null = True, blank = True)
+    sinopse = models.TextField()
+    data_cadastro = models.DateTimeField(default= timezone.now)
+
+    def __str__(self):
+        return self.nome
+
 
 
 # Filme - Nome do Filme, ano de lançamento, estúdio, gênero, sinopse, data de cadastro
-# Gênero - nome. e
+# Gênero - nome.
 
